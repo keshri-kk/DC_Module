98f1f30e42ce7a85eb3509d8af25824e96776e72
alkesh-harde <harde.d.alkesh@gmail.com>
2024-02-25 11:57:53 -0800

    commit by Alkesh

--- src/main/java/com/DataCollection/DC_Module/DcModuleApplication.java
+++ src/main/java/com/DataCollection/DC_Module/DcModuleApplication.java
@@ -11,12 +11,7 @@ public class DcModuleApplication {
 
 		System.out.println("Application Running");
 		System.out.println("This is Keshri");
-
-		System.out.println("Good Morning ");
-
-
-
-
+		System.out.println("Hii");
 	}
 
 }
