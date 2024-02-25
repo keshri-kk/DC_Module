b5e22cc5ebf18c7c43e47b9ae28b78f5b69c9e34
Keshri <kk28011997@gmail.com>
2024-02-25 11:13:58 +0530

    Creating Git Repository

---
+++ src/main/java/com/DataCollection/DC_Module/DcModuleApplication.java
@@ -0,0 +1,15 @@
+package com.DataCollection.DC_Module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcModuleApplication.class, args);
+
+		System.out.println("Application Running");
+	}
+
+}
---
+++ src/test/java/com/DataCollection/DC_Module/DcModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DataCollection.DC_Module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
